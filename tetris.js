f3a0fe6beb292d4d47d14d7355315ae8003e8379
SofiaSSR <unitargetofsofi@gmail.com>
2020-11-08 19:25:34 -0500

    merged fully

--- tetris.js
+++ tetris.js
@@ -21,8 +21,10 @@ function genPoly(n) {
   colores.shift();
   }
 class tablero{
-constructor(A,B){
+constructor(A,B,l1,c1,c2,c3){
 this.matriz = Array.from({length: A},()=>Array.from({length: B},()=>0));
+this.color = [c1,c2,c3];
+this.fondo = l1;
 }
 completo(){//proceso para limpiar completar filas  
   let filled_lines_counter = 0;  // Has the count of the removed lines in the current game
@@ -37,15 +39,16 @@ completo(){//proceso para limpiar completar filas
     next(filled_lines_counter);
   }
   show(guide_point){
+    stroke(this.color);
     this.matriz.forEach((row,y)=>{
      row.forEach((value,x)=>{
+       fill(this.fondo);
       if(value!==0)
         fill(colores[value-1]);
-      stroke(125, 213, 200);
       rect(tile_width*(x)+guide_point[0],tile_height*(y)+guide_point[1],tile_width,tile_height);
-      noFill();
-          
-    })})}
+    })})
+    noFill();}
+    
 }
 class poliomino {
 
@@ -114,9 +117,12 @@ class poliomino {
          tableu.matriz[ry][rx]=this.id;})
    return bol;}
 }
-var ancho = 500, alto = 550;
-var inicialc =[ancho*6/48,alto*2/30];
-var inicialn =[ancho*9/12,alto*13/15];
+const ancho = 500, alto = 550;
+const inicialc =[ancho*6/48,alto*2/30];
+const inicialn =[ancho*9/12,alto*13/15];
+const boardcol =[0];
+const curcol=[125,125,213,200];
+const  fastcol=[200,240,240,25];
 var tiempos={transcurrido:0,ultimaToma:0,active:700,leveled:700,fast:100};//tiempo transcurrido desde la ultima toma, tultima toma,caida,caida normal,caida fast-mode;
 var estado = ["inicio"];
 var medidas ={lineas:0,nivel:1,score:0};
@@ -125,9 +131,12 @@ var game_lives = 3;
 var update_board_flag = true;
 var juego_rapido = false;
 var lost_game = false;
-var score_copy = 0;
-var copy_board=[];
-var tile_width,tile_height;
+var score_copy = 0;//la copia del score antes de iniciar el fast mode
+var copy_board=[];//la copia del tablero antes de iniciar el fast mode
+var tile_width,tile_height;//ancho y alto de cada cuadro
+let img;
+var fast_game_counter = -1;  // stores the number of times fast_game has occurred
+var fast_game_counter_flag = false;  // For copying the board just once
 //-----------------funciones determinantes en la funcion del juego---------------------
   function determinado(){//devuelve las variables a su estado original (posibilita un rerun sin actualizar pagina)
    medidas ={lineas:0,nivel:1,score:0};
@@ -139,6 +148,8 @@ var tile_width,tile_height;
    juego_rapido = false;
    update_board_flag = true;
    game_lives = 3;
+   fast_game_counter = -1;
+   fast_game_counter_flag = false;
   }
   function next(cuanto){//mira que hacer cuando se limpia una fila (suma puntaje, da vidas etc)
     if (cuanto == elene ||cuanto == elene-1){
@@ -158,11 +169,28 @@ var tile_width,tile_height;
    textAlign(CENTER,TOP);
    text(letra,width*10/12,height*pt2/15);
     text(letra2,width*10/12,height*(pt2+1)/15);}
+
+  function displayLives(){
+      switch(game_lives){
+        case 1:
+          image(img, 340, 120, 50, 50);
+          break;
+        case 2:
+          image(img, 340, 120, 50, 50);
+          image(img, 390, 120, 50, 50);
+          break;
+        case 3:
+          image(img, 340, 120, 50, 50);
+          image(img, 390, 120, 50, 50);
+          image(img, 440, 120, 50, 50);
+      }
+    }
   
   function setup() {
     var canvas= createCanvas(500,550);
     canvas.parent("canvas");
     rectMode(CENTER);
+    img = loadImage('hearth_image_file.png');
     noLoop();
   }
   
@@ -170,7 +198,8 @@ function draw() {
   if(estado[0]=="inicio"){
     inicio();
    }else if(estado[0]=="juego"){
-     juego(curcol[0],curcol[1],curcol[2],curcol[3],1);
+     juego();
+     displayLives();
      fast_game();
      if(tableu.matriz[0].some((value)=>{return value!=0})){  // A tile reaches top
        game_lives--;
@@ -179,6 +208,7 @@ function draw() {
         tableu.matriz = JSON.parse(JSON.stringify(copy_board));
          medidas.score = score_copy.valueOf();
          lost_game = true;
+
        }
        else if (game_lives == 0 || !(juego_rapido))
          gameover();
@@ -189,7 +219,7 @@ function draw() {
   function start(){
    estado[0]="juego";
    document.getElementById("inicio").style.display = "none";
-   tableu = new tablero(dimta[2],dimta[0]);
+   tableu = new tablero(dimta[2],dimta[0],curcol[0],curcol[1],curcol[2],curcol[3]);
    let a = Math.floor(Math.random() * tetro.length);
    current = new poliomino(Math.round(dimta[0]/2)-1,false,tetro[a],0,a);
    a= Math.floor(Math.random() * tetro.length);
@@ -199,7 +229,7 @@ function draw() {
    loop();
   }  
   function inicio(){
-      background(0);
+      background(boardcol);
       //musica.play();
       stroke(255);
       textSize(20);
@@ -207,22 +237,18 @@ function draw() {
       text("Presiona para comenzar", width/2,height*1/4);
       text("Records",width/2,height/2);
     }
-  var curcol=[125,125,213,200];
-  fastcol=[];
-  function juego(fondo,c1,c2,c3,stroken){
-   background(0);
+  function juego(){
+   background(boardcol);
    stroke(255);
    textito("TETRIS","",2,35);
    textito("SCORE",medidas.score,5,25);
    textito("LEVEL",medidas.nivel,8,15);
    textito("LINES",medidas.lineas,10,15);
    textito("NEXT","",12,15);
-   fill(fondo);
+   fill(tableu.fondo);
    rectMode(CORNERS);
-   strokeWeight(stroken);
-   stroke(c1,c2,c3);
+   stroke(tableu.color);
    rect(inicialc[0]-25/2,inicialc[1]-13,inicialc[0]+dimta[1]-25/2,inicialc[1]+dimta[3]-13);
-   strokeWeight(1);
    rectMode(CENTER);
    //end default
   tableu.show(inicialc);
@@ -231,17 +257,20 @@ function draw() {
   current.update();
   }
   function fast_game(){
-    if (medidas.score >= 500 && medidas.score <= 5000)
+    if (tiempos.ultimaToma >= 5000*(4+(5*fast_game_counter)) && tiempos.ultimaToma < (30000*(fast_game_counter+1)))  // fast game interval
     {
+      console.log("fast");
       if (update_board_flag && (!lost_game))  // Create a copy board
       {
         score_copy = medidas.score.valueOf();
         copy_board = JSON.parse(JSON.stringify(tableu.matriz));
         console.log(copy_board);
         update_board_flag = false;
+        juego_rapido = true;
+        tiempos.active= tiempos.fast;
+        tableu.color=[fastcol[1],fastcol[2],fastcol[3]];
+        tableu.fondo=fastcol[0];
       }
-      juego_rapido = true;
-      tiempos.active= tiempos.fast;
     }
     else
     {
@@ -249,6 +278,12 @@ function draw() {
       juego_rapido = false;
       tiempos.active = tiempos.leveled;
       update_board_flag = true;
+      tableu.color=[curcol[1],curcol[2],curcol[3]];
+      tableu.fondo=curcol[0];
+      if (!fast_game_counter_flag) {
+        fast_game_counter++;
+        fast_game_counter_flag = true;
+      }
     }
 }
   function keyPressed(){//deteccion de teclas
@@ -280,7 +315,7 @@ function draw() {
       tableu.completo();}}
   
   function gameover(){//stylish del gameover
-    background(0);
+    background(boardcol);
     stroke(255);
     estado[0]="gameover";
     textSize(30);
